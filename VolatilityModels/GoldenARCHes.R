9cfea34eab90a3bdf48d4af7b0010379e559bb9e
bgilbert10 <bgilbert10@yahoo.com>
2022-11-21 11:42:31 -0700

    Update a few lines of code

--- VolatilityModels/GoldenARCHes.R
+++ VolatilityModels/GoldenARCHes.R
@@ -7,6 +7,8 @@ require(tseries)
 require(vars)
 require(fGarch)
 
+# 1) Organize data ----------------------
+
 # Read data from Yahoo!Finance
 getSymbols("GC=F")
 head(`GC=F`)
@@ -17,6 +19,8 @@ gold <- gold[paste("2007-01-01","2021-12-05",sep="/")]
 
 goldrtn <- diff(log(gold[,1]))
 ts.plot(goldrtn) # obvious volatility clustering
+
+# 2) ARIMA model for the mean ---------------------------
 
 # get a model for the mean that handles autocorrelation, calculate the residuals. 
 auto.arima(goldrtn) 
@@ -31,7 +35,9 @@ tsdisplay(res)
 Box.test(res,lag=10,type='Ljung')
 Box.test(res,lag=30,type='Ljung')
 
-# clear autocorrelation in squared residuals
+# 3) GARCH modeling -------------------------
+
+# There is clear autocorrelation in squared residuals
 res2 = res^2
 acf(res2)
 pacf(res2)
@@ -44,6 +50,9 @@ goldrtn <- na.remove(goldrtn)
 # Note garch order is reversed (sig_t AR(1) comes 2nd, sig_t MA(2) comes first)
 GoldenArch = garchFit(~arma(1,2)+garch(2,1),data=goldrtn,trace=F,cond.dist='sstd',include.mean=T)
 
+
+# 4) Evaluate GARCH model ------------------
+
 # still some autocorrelation in squared residuals
 summary(GoldenArch)
 
@@ -51,13 +60,13 @@ summary(GoldenArch)
 tsdisplay(residuals(GoldenArch))
 acf(GoldenArch@sigma.t)
 
-# mu, ma1 to ma3 are for the mean. 
+# mu, ar1, ma1 to ma2 are for the mean. 
 # omega, alpha (ARCH coefficents), beta (GARCH coefficients)
-# r(t) = 0.000196 + a(t) - 0.00549*a(t-1) + 0.00752*a(t-2) + 0.004*a(t-3)
+
+# r(t) = 0.00055 - 0.902*r(t-1) + a(t) + 0.872*a(t-1) - 0.0312*a(t-2)
 
 # model for the volatility:
-# sig^2(t) = 0.0000012 + 0.0179*a^2(t-1) + 0.036*a^2(t-2) + 
-#                           0.938*sig^2(t-1) 
+# sig^2(t) = 0.00000063 + 0.96*sig^2(t-1) + 0.013*a^2(t-1) + 0.023*a^2(t-2) + 
 
 # notice how many options with plot
 plot(GoldenArch)
